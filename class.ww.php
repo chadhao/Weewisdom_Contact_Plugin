6cee0376096e6b5be8b2b22402451d5abcc1f148
ArrisLee <arris_li@hotmail.com>
2016-07-02 23:27:42 +1200

    First Commit

---
+++ class.ww.php
@@ -0,0 +1,110 @@
+<?php
+
+class WW_Management
+{
+
+    public static function ww_init()
+    {
+        /*
+        add_action('admin_post_activity_signup', array('Activity_Signup', 'activity_signup_frontend_add'));
+        add_action('admin_post_nopriv_activity_signup', array('Activity_Signup', 'activity_signup_frontend_add'));
+        if (!self::$activity_initialed) {
+            self::activity_init_hooks();
+        }
+        */
+    }
+
+    public static function ww_activation()
+    {
+        self::ww_init_database();
+    }
+
+    public static function ww_deactivation()
+    {
+    }
+
+    public static function ww_uninstall()
+    {
+    }
+
+    /**
+     * Determining if required table exists.
+     *
+     * @global type $wpdb
+     *
+     * @return bool
+     */
+    /*
+    private static function activity_is_table_created( $activity_table_name ) {
+        global $wpdb;
+        $activity_table_dbname = DB_NAME;
+        $activity_table_sql = "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '$activity_table_dbname' AND table_name = '$activity_table_name'";
+        $activity_table_count = $wpdb->get_var( $activity_table_sql );
+
+        if ( $activity_table_count > 0 ) {
+            return true;
+        }
+        return false;
+    }
+    */
+
+    /**
+     * Initialize plugin database.
+     */
+    private static function ww_init_database()
+    {
+        global $wpdb;
+        $ww_table_name_center = $wpdb->prefix.'ww_center';
+        $ww_table_name_student = $wpdb->prefix.'ww_student';
+        $ww_charset_collate = $wpdb->get_charset_collate();
+
+        $ww_center_sql = "CREATE TABLE $ww_table_name_center (
+			center_id int(5) unsigned NOT NULL AUTO_INCREMENT,
+			name varchar(255) NOT NULL,
+			email varchar(255) NOT NULL,
+			phone varchar(15) NOT NULL,
+            address varchar(255) NOT NULL,
+            scale int(10),
+            current int(10),
+            is_full boolean DEFAULT false NOT NULL,
+			CONSTRAINT pk_center_id PRIMARY KEY (center_id),
+			CONSTRAINT fk_center_id FOREIGN KEY (center_id) REFERENCES ".$ww_student."(center_id)
+		) $ww_charset_collate;";
+
+         $ww_student_sql = "CREATE TABLE $ww_table_name_student (
+            std_id int(10) unsigned NOT NULL AUTO_INCREMENT,
+            name varchar(255) NOT NULL,
+            email varchar(255) NOT NULL,
+            phone varchar(15) NOT NULL,
+            address varchar(255) NOT NULL,
+            age int(5),
+            gender boolean DEFAULT false NOT NULL, /*false = male, true = female*/
+            /*
+            homebase_id,
+            exchange_id,
+            */
+            center_id int(5) unsigned,
+            is_enrolled boolean DEFAULT false NOT NULL,
+            CONSTRAINT pk_std_id PRIMARY KEY (std_id)
+        ) $ww_charset_collate;";
+
+        require_once ABSPATH.'wp-admin/includes/upgrade.php';
+        dbDelta($ww_center_sql);
+        dbDelta($ww_student_sql);
+
+    }
+
+    /**
+     * Initialize wordpress hooks.
+     */
+    private static function ww_init_hooks()
+    {
+    }
+
+    /*
+    public static function ww_view($file_name)
+    {
+        include ACTIVITY__PLUGIN_DIR.'views/'.$file_name.'.php';
+    }
+    */
+}
---
+++ ww.php
@@ -0,0 +1,41 @@
+<?php
+/*
+Plugin Name: WW_Management
+Plugin URI: http://465.co.nz/
+Description: This is the Activity plugin developed specially for Wee Wisdom.
+Version: 0.1
+Author: Arris, Rick, Teddy, Chad
+License: GPLv2
+Text Domain: WW
+*/
+
+// This file should not be called directly
+if (!function_exists('add_action')) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+}
+
+define('WW_Management_URL', plugin_dir_url(__FILE__));
+define('WW_Management_DIR', plugin_dir_path(__FILE__));
+
+register_activation_hook(__FILE__, array('WW_Management', 'ww_activation'));
+register_deactivation_hook(__FILE__, array('WW_Management', 'ww_deactivation'));
+
+require_once WW_Management_DIR.'class.ww.php';
+//require_once ACTIVITY__PLUGIN_DIR.'class.activity-signup.php';
+
+add_action('init', array('WW_Management', 'ww_init'));
+
+/*
+if (is_admin()) {
+    require_once ACTIVITY__PLUGIN_DIR.'class.activity-admin.php';
+    add_action('init', array('Activity_Admin', 'activity_admin_init'));
+}
+*/
+
+// Code blow enables debug message output during plugin activation.
+// add_action('activated_plugin', 'my_save_error');
+// function my_save_error()
+// {
+//     file_put_contents(ACTIVITY__PLUGIN_DIR.'error_activation.html', ob_get_contents());
+// }
