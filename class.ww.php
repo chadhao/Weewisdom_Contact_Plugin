c36686d8fbe2d11d331a596af932a6e9b7b70788
ArrisLee <arris_li@hotmail.com>
2016-07-06 22:31:22 +1200

    test add enquiry

--- class.ww.php
+++ class.ww.php
@@ -41,6 +41,12 @@ class WW_Module
         );
     }
 
+    public static function ww_del_enquiry()
+    {
+        global $wpdb;
+        $wpdb->delete('wp_ww_enquiry', array( 'ID' => 1 ) );
+    }
+
     public static function ww_show_enquiry()
     {
         global $wpdb;
@@ -90,7 +96,7 @@ class WW_Module
     {
         add_menu_page('WeeManager', 'WeeManager', 'edit_pages', 'Wee_Menu', array('WW_Module', 'ww_update_center'), 'dashicons-smiley', 2);
         add_submenu_page('Wee_Menu', 'WeeCenter', 'WeeCenter', 'edit_pages', 'Wee_Menu', array('WW_Module', 'ww_show_enquiry'));
-        add_submenu_page('Wee_Menu', 'WeeEnquiry', 'WeeEnquiry', 'edit_pages', 'SubWeeEnquiry', array('WW_Module', 'ww_update_enquiry'));
+        add_submenu_page('Wee_Menu', 'WeeEnquiry', 'WeeEnquiry', 'edit_pages', 'SubWeeEnquiry', array('WW_Module', 'ww_del_enquiry'));
     }
 
     /**
