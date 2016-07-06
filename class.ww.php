c9ec26d7433d245c45a9d7e62f1356b988da278d
ArrisLee <arris_li@hotmail.com>
2016-07-06 21:55:48 +1200

    test update

--- class.ww.php
+++ class.ww.php
@@ -30,7 +30,7 @@ class WW_Module
         $wpdb->update('wp_ww_center', 
             array( 'name' => 'cool',  'phone' => '1111111111'), 
             array( 'center_id' => 1 ), 
-            array( '%s', '%s'),
+            array( '%s', '%s')
         );
     }
 
