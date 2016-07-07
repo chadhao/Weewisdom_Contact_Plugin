5859259f2d1e9f4d0898cec888f48b6955078262
rickdrew <104257856@qq.com>
2016-07-07 22:19:48 +1200

    update view

--- view/center_view.php
+++ view/center_view.php
@@ -1,10 +1,28 @@
 <?php
-$center_list = WW_Module::ww_show_center();
+$center_list = WW_Module::ww_get_center();
 if(!$center_list)
 {
 	echo "Can not identify any center information!";
 }
 ?>
 <div>
-  
- </div>
+  <table>
+    <tr>
+      <td>center_id</td>
+      <td>name</td>
+      <td>email</td>
+      <td>phone</td>
+      <td>address</td>
+    </tr>
+    <?php
+      foreach ($list as $piece) {
+        echo '<tr>'.
+        '<td>'.$piece->center_id.'</td>'.
+        '<td>'.$piece->name.'</td>'.
+        '<td>'.$piece->email.'</td>'.
+        '<td>'.$piece->phone.'</td>'.
+        '<td>'.$piece->address.'</td>'.
+      }
+     ?>
+  </table>
+</div>
