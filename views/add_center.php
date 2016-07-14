a741fcd9507e273abc8da20d2afb4abe51ed43e3
ArrisLee <arris_li@hotmail.com>
2016-07-14 23:12:57 +1200

    Test add_center routing -8

--- views/add_center.php
+++ views/add_center.php
@@ -1,4 +1,4 @@
-<a href="<?php echo esc_url(WW_Module::ww_manage_get_url('add_center')); ?>" class="page-title-action">ADD CENTER</a>
+<a href="<?php echo esc_url(WW_Module::ww_manage_get_url('activity_admin_add_post')); ?>" class="page-title-action">ADD CENTER</a>
 
 
 
