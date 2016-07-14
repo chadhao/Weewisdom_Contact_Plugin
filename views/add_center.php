6a71d680ad454449b604eec1fab8d3a091129519
ArrisLee <arris_li@hotmail.com>
2016-07-14 22:40:59 +1200

    Test add_center routing -4

--- views/add_center.php
+++ views/add_center.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-<h1>活动列表 <a href="<?php echo esc_url(WW_Module::ww_manage_get_url('add_center')); ?>" class="page-title-action">添加活动</a></h1>
+<a href="<?php echo esc_url(WW_Module::ww_manage_get_url('activity_admin_add_post')); ?>" class="page-title-action">ADD CENTER</a>
 </div>
 
 
