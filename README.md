f8cc42e9aebed543dd3f575962263a4468a6cbeb
ArrisLee <arris_li@hotmail.com>
2016-07-18 21:28:04 +1200

    load menu slug modify

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# WordPress_Plugin_AUTCSA_activity
-This is the Activity plugin developed specially for autcsa.org.nz.
+# Weewisdom_Contact_Plugin
+Weewisdom_Contact_Plugin
--- activity/views/style.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo '<style type="text/css"> @import url("https://cdnjs.cloudflare.com/ajax/libs/amazeui/2.5.2/css/amazeui.min.css"); </style>';
-
-echo '<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/amazeui/2.5.2/js/amazeui.min.js"></script>';
-?>
