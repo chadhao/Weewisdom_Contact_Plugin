2854445324383da411e7b76e306dac8ed2e6f70b
ArrisLee <arris_li@hotmail.com>
2016-07-27 22:06:46 +1200

    add center test 27/07/2016

--- views/add_center.php
+++ views/add_center.php
@@ -1,6 +1,6 @@
 <form class="am-form" method="post" action="<?php echo esc_url(WW_Module::ww_manage_get_url('add_center'));?>">
   Title:<br>
-  <input type="text" name="name"><br>
+  <input type="text" name="name"><br><br>
   Address:<br>
   <input type="text" name="address"><br><br>
   Email:<br>
