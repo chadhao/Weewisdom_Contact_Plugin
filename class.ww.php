c68950a1c945697ad9e748792d3bea819c1db708
ArrisLee <arris_li@hotmail.com>
2016-07-27 23:25:36 +1200

    add center test 27/07/2016

--- class.ww.php
+++ class.ww.php
@@ -86,7 +86,7 @@ class WW_Module
              array('name' => $input['name'], 'email' => $input['email'], 'phone' => $input['phone'], 'address' => $input['address']),
             array('%s', '%s', '%s', '%s')
         );
-        include WW_Management_DIR.'views/list_center.php';
+        //include WW_Management_DIR.'views/list_center.php';
     }
 
     public static function ww_del_center()
