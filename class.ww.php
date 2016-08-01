edb07509dfa9ec1a17a885bbdb0fba572219b994
ArrisLee <arris_li@hotmail.com>
2016-08-01 21:50:07 +1200

    test center func 01/08/2016

--- class.ww.php
+++ class.ww.php
@@ -107,7 +107,9 @@ class WW_Module
         $wpdb->insert('wp_ww_center',
            array('name' => $input['name'], 'email' => $input['email'], 'phone' => $input['phone'], 'address' => $input['address']),
            array('%s', '%s', '%s', '%s'));
-           header("location: ".self::ww_manage_get_url());
+        echo '<script>alert("!")</script>';
+        header("location: ".self::ww_manage_get_url());
+        exit();
            //self::ww_view('list_center');
     }
 
