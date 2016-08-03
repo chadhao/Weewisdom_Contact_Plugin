37fa68bc3beb328ab2910714204648d035a2ef4c
ArrisLee <arris_li@hotmail.com>
2016-08-03 20:52:21 +1200

    Center action test 03/08/2016

--- views/del_center.php
+++ views/del_center.php
@@ -1,4 +1,4 @@
-<form method="post" action="<?php echo esc_url(WW_Module::ww_manage_get_url('del_center'));?>">
+<form method="post" action="<?php echo esc_url(WW_Module::ww_manage_get_url('process_del_center'));?>">
   Title:<br>
   <input type="text" name="name"><br><br>
   <input type="submit" value="DELETE">
