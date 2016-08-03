fe6755cf71d3b86ebb0b9f69704e3dd3575d66e1
ArrisLee <arris_li@hotmail.com>
2016-08-03 21:14:46 +1200

    Center action test 03/08/2016

--- class.ww.php
+++ class.ww.php
@@ -139,7 +139,7 @@ class WW_Module
     }
 
 
-    public static function ww_manage_get_url($action)
+    public static function ww_manage_get_url($action, $center_id = 0)
     {
         //default menu page
         if (!$action)
@@ -157,12 +157,12 @@ class WW_Module
 
         //delete center pages
         if ($action == 'del_center') {
-            $args = array('page' => 'cen_action', 'action' => $action, '_wpnonce' => wp_create_nonce(self::NONCE));
+            $args = array('page' => 'cen_action', 'action' => $action, 'center_id' => $center_id, '_wpnonce' => wp_create_nonce(self::NONCE));
         }
 
         //update center pages
         if ($action == 'update_center') {
-            $args = array('page' => 'cen_action', 'action' => $action, '_wpnonce' => wp_create_nonce(self::NONCE));
+            $args = array('page' => 'cen_action', 'action' => $action, 'center_id' => $center_id, '_wpnonce' => wp_create_nonce(self::NONCE));
         }
 
         $url = add_query_arg($args, admin_url('admin.php'));
