19368823524a7e3d3e4bc2b24a9f2e404245e205
ArrisLee <arris_li@hotmail.com>
2016-08-03 23:47:01 +1200

    Center action test 03/08/2016

--- views/list_center.php
+++ views/list_center.php
@@ -2,9 +2,9 @@
 include 'style.php';
 $center_list = WW_Module::ww_get_center();
 
-if (!$center_list) {
+if (!$center_list) 
+{
   echo '<div class="error"><p>No recorded center identified!</p></div>';
-  //echo 'Can not identify any center information!';
 }
 else
 {
