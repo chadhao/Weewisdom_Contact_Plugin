c7e155f81220452895e6cbdb683979464c1fc74f
rickdrew <104257856@qq.com>
2016-08-04 15:29:21 +1200

    update 15.29

--- class.ww.php
+++ class.ww.php
@@ -45,7 +45,7 @@ class WW_Module
         global $wpdb;
         $input = array('name' => $_POST["name"], 'email' => $_POST["email"], 'phone' => $_POST["phone"], 'center_id' => $_GET["center_id"], 'is_contacted' => $_POST["is_contacted"]);
         $wpdb->insert('wp_ww_enquiry',
-        array('name' => $input['name'], 'email' => $input['email'], 'phone' => $input['phone'], 'center_id' => $input['center_id'], 'is_contacted' => $input['is_contacted']),
+        array('name' => $input['name'], 'email' => $input['email'], 'phone' => $input['phone'], 'center_id' => $input['center_id'], 'is_contacted' => $input['is_contacted'])
       );
         self::ww_view('list_enquiry');
     }
