bc84e37faff8d9d1f8ce42d80ca2710a4f91cb7a
rickdrew <104257856@qq.com>
2016-08-04 15:24:32 +1200

    update 15.24

--- views/add_enquiry.php
+++ views/add_enquiry.php
@@ -1,8 +1,6 @@
-<?php include 'style.php';
-
-?>
+<?php include 'style.php';?>
 <div class="wrap">
-  <form class="am-form am-form-horizontal" method="post" action="<?php echo esc_url(WW_Module::ww_manage_get_url('add_enquiry');?>">
+  <form class="am-form am-form-horizontal" method="post" action="<?php echo esc_url(WW_Module::ww_manage_get_url('add_enquiry'));?>">
     <fieldset>
     <div class="am-form-group">
        <label for="name" class="am-u-sm-1 am-form-label">Name</label>
