07c64b03974504cfa66950d15fd1a17fe11a6a3a
rickdrew <104257856@qq.com>
2016-08-05 12:41:57 +1200

    update and del enquiry 12.41

--- class.ww.php
+++ class.ww.php
@@ -274,14 +274,14 @@ class WW_Module
             $args = array('page' => 'enq_action', 'action' => $action, 'center_id' => $center_id, '_wpnonce' => wp_create_nonce(self::NONCE));
         }
 
-        if ($action == 'show_update_enquiry') {
+/*        if ($action == 'show_update_enquiry') {
             $args = array('page' => 'enq_action', 'action' => $action, 'center_id' => $center_id, 'enq_id' => $enq_id, '_wpnonce' => wp_create_nonce(self::NONCE));
         }
 
         if ($action == 'del_enquiry') {
             $args = array('page' => 'enq_action', 'action' => $action, 'center_id' => $center_id, 'enq_id' => $enq_id, '_wpnonce' => wp_create_nonce(self::NONCE));
         }
-
+*/
         $url = add_query_arg($args, admin_url('admin.php'));
 
         return $url;
