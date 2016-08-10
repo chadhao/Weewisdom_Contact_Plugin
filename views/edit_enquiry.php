d230a9d4d3e9d6d190a494191e4d5a82c88a075a
rickdrew <104257856@qq.com>
2016-08-10 22:42:10 +1200

    update 22.42

--- views/edit_enquiry.php
+++ views/edit_enquiry.php
@@ -28,7 +28,7 @@
            <input type="checkbox" name="is_contacted" checked="<?php echo ($_SESSION['is_contacted'] == 1 ? 'checked' : '');?>">
        </div>
     </div>
-
+    <p3><?php echo ($_SESSION['is_contacted']);?></p3>
     <div class="am-form-group">
        <div class="am-u-sm-11 am-u-sm-offset-1">
            <button type="submit" class="am-btn am-btn-primary am-radius">Submit</button>
